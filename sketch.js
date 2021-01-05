ca19bd2b86ec884cb6874664e7f9d920a3c187ba
spandanbasu2009 <74770709+spandanbasu2009@users.noreply.github.com>
2021-01-05 16:59:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var bulletPart1,bulletPart2;
+var wall,thickness;
+var speed, weight;        
+
+function setup() {
+    createCanvas(1600,400);
+ 
+    speed = random(223,321);
+    weight = random(30,52);
+    thickness = random(22,83)
+
+    bulletPart1 = createSprite(100,200,100,20);
+    bulletPart1.velocityX = speed;
+    bulletPart2 = createSprite(bulletPart1.x+40,bulletPart1.y,20,20);
+    bulletPart2.velocityX = bulletPart1.velocityX;
+    bulletPart1.shapeColor = color("pink");
+    bulletPart2.shapeColor = color("orange");
+    wall = createSprite(1200,200,thickness,height/2);
+    wall.shapeColor = color("yellow");
+}
+
+function draw() {
+       background("blue");
+
+     if(wall.x-bulletPart1.x < (bulletPart1.width+wall.width)/2){
+         bulletPart1.velocityX=0;
+         bulletPart2.velocityX = 0;
+         var damage = (0.5*weight*speed*speed)/(wall.width*wall.width*wall.width);
+
+         if(damage > 10){
+             bulletPart1.shapeColor = color("green");
+             bulletPart2.shapeColor = color("red");
+         }
+         //if(deformation < 180 && deformation > 100){
+             //car.shapeColor = color(230,230,0);
+         //}
+         if(damage < 10){
+             bulletPart1.shapeColor = color("red");
+             bulletPart2.shapeColor = color("green");
+         }
+     }
+
+
+    drawSprites();
+}
